43258db7e888bdaf0d0f917c3959dee7f291c2e7
Bucko Akos <akosbucko@gmail.com>
2020-05-10 14:52:51 +0200

    overlapping rectangles

--- app.js
+++ app.js
@@ -4,4 +4,18 @@ var context = canvas.getContext('2d');
 var canvasWidth = canvas.width;
 var canvasHeight = canvas.height;
 
-context.fillRect(canvasWidth / 2, canvasHeight / 2, canvasWidth / 2, canvasHeight / 2);
+var size = 100;
+var posX = canvasWidth / 2 * 0.75;
+var posY = canvasHeight / 2 * 0.75;
+
+context.fillStyle = "rgba(255,0,0, .5)";
+
+context.fillRect( posX, posY, size, size);
+
+posX += size *0.50;
+posY += size *0.50;
+
+context.fillStyle = "rgba(0,0,255, .5)";
+
+context.fillRect( posX, posY, size, size);
+
